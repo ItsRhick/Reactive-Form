27f3b34ff3e0d96509c33490fe051da4c9c7613f
Rik <145538186+ItsRhick@users.noreply.github.com>
2025-02-17 12:34:37 +0800

    app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,20 +8,43 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'reactive-form';
   userName: string = "";
+  
   formdata: FormGroup = new FormGroup({
     username: new FormControl(""),
-    email: new FormControl(""),
-    password: new FormControl(""),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8)]),
     confirmPassword: new FormControl("")
+  });
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)])
   });
 
   onClickSubmit(data: { username: string, email: string, password: string, confirmPassword: string }) {
     this.userName = data.username;
     console.log('Form submitted with:', data);
+  }
+
+  onSubmit() {
+    if (this.formdata.valid) {
+      this.onClickSubmit(this.formdata.value);
+      console.log('Registration Form Submitted', this.formdata.value);
+    } else {
+      console.log('Registration Form Not Valid');
+    }
+  }
+
+  onLoginSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login Form Submitted', this.loginForm.value);
+    } else {
+      console.log('Login Form Not Valid');
+    }
   }
 }
